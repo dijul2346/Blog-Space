8c869ed417c784198a4f32991bf1cac8077811f0
dijul2346 <146112003+dijul2346@users.noreply.github.com>
2025-07-04 22:56:53 +0530

    Connected to mongo db and added authentication

---
+++ Blog-Space/src/backend/index.cjs
@@ -0,0 +1,56 @@
+const path=require("path")
+const jwt=require("jsonwebtoken")
+const express=require("express")
+const app=express()
+app.use(express.json())
+const cors = require('cors');
+app.use(cors());
+const mongoose=require('mongoose')
+
+const { UserModel, BlogModel } = require("./mongo.cjs");
+
+mongoose.connect("mongodb+srv://dijulM:DM%40ncr70@cluster0.fezmq8v.mongodb.net/Blog-app-db");
+console.log(mongoose.connection.readyState);
+
+
+app.post("/signup", function(req, res) {
+    const { name, userName, password } = req.body;
+    UserModel.findOne({ userName })
+        .then(function(user) {
+            if (user) {
+                res.send("username exists");
+            } else {
+                UserModel.create({
+                    name: name,
+                    userName: userName,
+                    password: password
+                }).then(() => {
+                    res.send("success");
+                });
+            }
+        })
+        .catch(err => res.status(500).send("error"));
+});
+
+
+app.post("/signin",function(req,res){
+    console.log("reached signin")
+    const userName=req.body.userName;
+    const password=req.body.password;
+    UserModel.findOne({
+        userName:userName,
+        password:password
+    })
+    .then(function(user){
+        if(user){
+            const userId=user._id
+            const token=jwt.sign({userId,userName},"123random");
+            res.send({token:token})
+        }
+        else{
+            res.send("invalid");
+        }
+    })
+})
+
+app.listen(4000)
---
+++ Blog-Space/src/backend/mongo.cjs
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose")
+
+const UserSchema=({
+    name:String,
+    userName:String,
+    password:String,
+})
+
+const BlogSchema=({
+    userid:String,
+    userName:String,
+    title:String,
+    content:String,
+})
+
+
+
+module.exports={
+    UserModel:mongoose.model('users',UserSchema),
+    BlogModel:mongoose.model('blogs',BlogSchema)
+}
